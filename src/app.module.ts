e596afe3446494f2146901b8429e1e69ea774a0c
Kitti Verdal <kitti@jbl.no>
2019-06-08 17:20:49 +0200

    Code cleaned

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { JSSFLoginComponent } from './components/jssf-login/jssf-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JSSFLoginComponent
   ],
   imports: [
     BrowserModule,
